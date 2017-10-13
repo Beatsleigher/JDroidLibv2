bb9afef3db56963c30db99b420ada2fe89216c8f
Beatsleigher <Beatsleigher@gmail.com>
2017-10-13 15:40:29 +0200

    Finished implementing FileSystem class
    FileSystem class is fully implemented, more may be added in the future.
    
    === File Additions ===
    
    * FileCouldNotBePushedException.java
    Device-class exception that is thrown when a file/dir could not be pushed to the device.
    
    === File Mods ===
    
    * FileSystem.java
    Fully implemented.
    
    * AndroidDirectory.java
    getContents() no longer referrs to the FileSystem class
    Added required exceptions to method footprints
    
    * Permission.java
    Added methods to get permissions from ls output
    
    * IDirectory.java
    Added exceptions to method protos
    
    * IFileSystemEntry.java
    Added RegEx patterns
    Added "permission denied" constant

--- src/main/java/eu/casoftworks/jdroidlib/enums/Permission.java
+++ src/main/java/eu/casoftworks/jdroidlib/enums/Permission.java
@@ -1,6 +1,9 @@
 package eu.casoftworks.jdroidlib.enums;
 
+import eu.casoftworks.jdroidlib.interfaces.*;
+
 import java.util.*;
+import java.util.regex.*;
 
 /**
  * Defines different permissions found on a Linux file system.
@@ -81,6 +84,46 @@ public enum Permission {
                 return permission;
         }
         return null;
+    }
+
+    /**
+     * Gets a permission element from a permissions string, where
+     * the permission string must be three characters long, and must match
+     * {@link IFileSystemEntry#PERMISSION_PATTERN}
+     *
+     * @param threeChar The permission string. Must match {@link IFileSystemEntry#PERMISSION_PATTERN}
+     * @return The {@link Permission} representing the permission string passed.
+     */
+    public static Permission fromThreeChar(String threeChar) {
+        int octal = 0;
+        threeChar = threeChar.toLowerCase();
+
+        if (!threeChar.matches(IFileSystemEntry.PERMISSION_PATTERN))
+            throw new IllegalArgumentException("Permission string must be three characters long, consisting of R, W, X, or -!");
+
+        for (char _char : threeChar.toCharArray()) {
+            if (_char == '-')
+                octal = octal | 0;
+            else if (_char == 'w')
+                octal = octal | WriteOnly.octal;
+            else if (_char == 'r')
+                octal = octal | ReadOnly.octal;
+            else if (_char == 'x')
+                octal = octal | ExecuteOnly.octal;
+        }
+
+        return fromOctal(octal);
+
+    }
+
+    public static Permission[] fromLsOutput(String lsOutput) {
+        List<Permission> perms = new ArrayList<>();
+        Pattern pattern = Pattern.compile(IFileSystemEntry.PERMISSION_PATTERN);
+        Matcher matcher = pattern.matcher(lsOutput);
+        while (matcher.find())
+            perms.add(fromThreeChar(lsOutput.substring(matcher.start(), 3)));
+
+        return (Permission[])perms.toArray();
     }
 
 }
